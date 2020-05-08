0127ebc35d0bdf41f7b34a8d51241e176bb55efc
moby-docker <k.uver0927@gmail.com>
2020-05-08 17:10:45 +0900

    わかんねー

--- my-first-nuxt-app/plugins/axios.js
+++
@@ -1,8 +0,0 @@
-export default function({ $axios }) {
-  $axios.onRequest((config) => {
-    if (process.env.QIITA_TOKEN) {
-      config.headers.common['Authorization'] = process.env.QIITA_TOKEN
-    }
-    return config
-  })
-}
